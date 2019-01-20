b756165a93a9853fc367e719d6ae913854d6985f
006699-RD3(王裕勝) <vincent.wang@senao.com.tw>
2019-01-20 19:14:31 +0800

    Mvc5StarterProject

---
+++ Mvc5StarterProject/Mvc5StarterProject/Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc5StarterProject.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public string Index()
+        {
+            return "這是我的<b>預設<b>動作";
+        }
+
+        public string welcome()
+        {
+            return "這是歡迎的action";
+        }
+    }
+}
